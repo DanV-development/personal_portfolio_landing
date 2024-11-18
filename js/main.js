b1eb7a0e5bdc250bd3a22437e777c88561881178
Воробьев Данил Сергеевич <dvorobev@@ispras.ru>
2024-11-18 17:09:25 +0300

    The landing page is fully animated

--- js/main.js
+++ js/main.js
@@ -12,10 +12,12 @@ const rightEllipse = $('#right_hero_ellipse');
 const arrowDown = $('.hero__arrow-down');
 
 const leftImage = $('#hero_left_image');
-const rightImage = $('#hero_right_image')
+const rightImage = $('#hero_right_image');
+
+const leftPanel = gsap.utils.toArray('.content__left-info');
+const rightPanel = gsap.utils.toArray('.content__right-info');
 
 $(document).ready(function() {
-
     $('.hero__text h1 span').lettering();
 
     gsap.timeline()
@@ -98,35 +100,98 @@ $(document).ready(function() {
     );
 });
 
-gsap.fromTo(leftImage,
-    {
-        opacity: 1,
-        x: 0,
-    },
-    {
-        opacity: 0,
-        x: -100,
-        scrollTrigger: {
-            trigger: leftImage,
-            start: 'top',
-            end: 'bottom',
-            scrub: true,
+if (ScrollTrigger.isTouch !== 1) {
+    gsap.fromTo(leftImage,
+        {
+            opacity: 1,
+            x: 0,
+        },
+        {
+            opacity: 0,
+            x: -100,
+            scrollTrigger: {
+                trigger: leftImage,
+                start: 'top',
+                end: 'bottom',
+                scrub: true,
+            }
         }
-    }
-);
-gsap.fromTo(rightImage,
-    {
-        opacity: 1,
-        x: 0,
-    },
-    {
-        opacity: 0,
-        x: 100,
-        scrollTrigger: {
-            trigger: rightImage,
-            start: 'top',
-            end: 'bottom',
-            scrub: true,
+    );
+    gsap.fromTo(rightImage,
+        {
+            opacity: 1,
+            x: 0,
+        },
+        {
+            opacity: 0,
+            x: 100,
+            scrollTrigger: {
+                trigger: rightImage,
+                start: 'top',
+                end: 'bottom',
+                scrub: true,
+            }
         }
-    }
-);
+    );
+
+    leftPanel.forEach(item => {
+        gsap.fromTo(item,
+            {
+                opacity: 0,
+                x: -70,
+            },
+            {
+                opacity: 1,
+                x: 0,
+                scrollTrigger: {
+                    trigger: item,
+                    start: '-600',
+                    end: '-100',
+                    scrub: true,
+                }
+            }
+        );
+    });
+
+    rightPanel.forEach(item => {
+        gsap.fromTo(item,
+            {
+                opacity: 0,
+                x: 70,
+            },
+            {
+                opacity: 1,
+                x: 0,
+                scrollTrigger: {
+                    trigger: item,
+                    start: '-600',
+                    end: '-100',
+                    scrub: true,
+                }
+            }
+        );
+    });
+
+    $('#skills_header').lettering();
+    gsap.fromTo(
+        '#skills_header span',
+        {
+            y: '200%',
+            opacity: 0,
+        },
+        {
+            y: '0%',
+            opacity: 1,
+            ease: 'bounce.out',
+            duration: 1,
+            stagger: 0.1,
+            scrollTrigger: {
+                trigger: '#skills',
+                start: 'top 65%',
+                toggleActions: 'play none none reverse'
+            }
+        }
+    );
+} else {
+    $('.hero__arrow-down').remove();
+}
